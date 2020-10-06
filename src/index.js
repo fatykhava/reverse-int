2b75208e135ee015b0940bef979419269a027850
Anastasiya Fatykhava <fatykhava@yandex.ru>
2020-10-06 16:31:15 +0300

    init: start Reverse-Int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	let str = String(n);
+	let reverseInt = '';
+	if (str[0] === '-') {
+		for (let i = str.length - 1; i >= 1; i--) {
+			reverseInt += str[i];
+		}
+	}
+	else {
+		for (let i = str.length - 1; i >= 0; i--) {
+			reverseInt += str[i];
+		}
+	}
+
+	return Number(reverseInt);
 }
