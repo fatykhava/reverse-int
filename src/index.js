ab823fefb26376f7854400159673fe0dd33d0fce
Anastasiya Fatykhava <fatykhava@yandex.ru>
2021-02-28 21:13:40 +0300

    refactor: change reverse function

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
 module.exports = function reverse(n) {
-	let str = String(n);
-	let reverseInt = '';
-	if (str[0] === '-') {
-		for (let i = str.length - 1; i >= 1; i--) {
-			reverseInt += str[i];
-		}
-	}
-	else {
-		for (let i = str.length - 1; i >= 0; i--) {
-			reverseInt += str[i];
-		}
-	}
-
-	return Number(reverseInt);
+    return parseInt(String(n).split('').reverse().join(''));
 }
